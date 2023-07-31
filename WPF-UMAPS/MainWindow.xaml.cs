c1dbdf513a2772c129546afda3a686215a480217
DaniloSosa98 <danilososa98@gmail.com>
2023-07-31 16:34:41 -0600

    Cambios del prototipo a primer diseño

--- WPF-UMAPS/MainWindow.xaml.cs
+++ WPF-UMAPS/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_UMAPS
 {
@@ -28,6 +16,17 @@ namespace WPF_UMAPS
         private void ExitBtn_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void MiniBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
