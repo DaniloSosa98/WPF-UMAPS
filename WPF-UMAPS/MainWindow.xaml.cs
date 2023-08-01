c94c6c6c2e6cb60d493db6daf3460c0b6b3b1f59
DaniloSosa98 <danilososa98@gmail.com>
2023-08-01 15:30:22 -0600

    Añadi paths de cada app y pequeños cambios de diseño

--- WPF-UMAPS/MainWindow.xaml.cs
+++ WPF-UMAPS/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 
 namespace WPF_UMAPS
@@ -27,6 +28,33 @@ namespace WPF_UMAPS
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
+        }
+
+        private void pactware_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("\"C:\\Program Files (x86)\\PACTware Consortium\\PACTware 4.1\\APP\\PACTWARE.EXE\"");
+        }
+
+        private void meterview_Click(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Users\Public\Desktop\MeterView XL 2.1.0.lnk";
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+            
+        }
+
+        private void simatic_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("\"C:\\Program Files (x86)\\SIEMENS\\STEP7\\S7BIN\\S7tgtopx.exe\"");
+        }
+
+        private void dim_Click(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = @"C:\Users\Public\Desktop\Device Integration Manager.lnk";
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
         }
     }
 }
